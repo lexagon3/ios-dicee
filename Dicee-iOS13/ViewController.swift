53f4181e5bf291cb9a2b89fad358199fd6ec9e9c
lexagonal <16949155+lexagonal@users.noreply.github.com>
2022-02-07 08:06:09 -0800

    Randomize the dice on button click.

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
+    
+    let dice = [
+        UIImage(named: "DiceOne"),
+        UIImage(named: "DiceTwo"),
+        UIImage(named: "DiceThree"),
+        UIImage(named: "DiceFour"),
+        UIImage(named: "DiceFive"),
+        UIImage(named: "DiceSix"),
+    ]
+    
+    @IBAction func roll(_ sender: UIButton) {
+        diceImageViewOne.image = dice[ Int.random(in: 0...5) ]
+        diceImageViewTwo.image = dice[ Int.random(in: 0...5) ]
     }
-
-
 }
 
